fce3348bcb16791e5c435a1ae401f130961a575f
Tanish-Eagle <ta.shrivastava@gmail.com>
2024-01-27 15:26:53 +0530

    Resolved the log out bug

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+
+# from django.contrib.auth import views as auth_views
 from .views import SignUpView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    # path("logout/", auth_views.LogoutView.as_view(next_page="home"), name="logout"),
 ]
